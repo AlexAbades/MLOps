a6b23591c9930e3a437e9d3e1114d03122c96dbf
AlexAbades <abadesalex31@gmail.com>
2023-01-08 21:55:01 +0100

    Data load, model creation, data file created, main script done with the trining

--- S1/final_exercise/data.py
+++ S1/final_exercise/data.py
@@ -1,9 +1,51 @@
 import numpy as np
 import torch
+from torchvision import transforms
 
 
-def mnist():
-    # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
-    return train, test
+def mnist(batch_size:int=10):
+
+    # specify the path where the data is stored 
+    files_folder = "./data/"
+    # Iterate through all the files to have the path 
+    train_files = [f"{files_folder}train_{i}.npz" for i in range(5)]
+    # Initialize lists
+    x_train = []
+    y_train = [] 
+    # Load all the data and put it in the same list 
+    for files in train_files:
+        with np.load(files) as load:
+            # Load the images
+            x_train.extend(load['images'])
+            # Load the labels
+            y_train.extend(load['labels'])
+    
+    # Tramform into tensors 
+    x_train = torch.Tensor(x_train)
+    y_train = torch.Tensor(y_train)
+
+    # Specify the test file, in this case just one 
+    test_file = "test.npz"
+    # Initialize lists 
+    with np.load(files_folder + test_file) as load:
+        x_test = load["images"]
+        y_test = load["labels"]
+
+    # Tranform into tensors 
+    x_test = torch.Tensor(x_test)
+    y_test = torch.Tensor(y_test)
+
+    # Defina a Normalization term to deal with the corrupted data 
+    # Mean 0.5 std 0.5 in first chanel
+    trans = transforms((0.5,), (0.5,))
+
+    # Create a container that stores a collection of tensors in a single object.
+    train_set = torch.utils.data.TensorDataset(trans(x_train), y_train)
+    # Create a data iterator that provides a way to batch and shuffle the examples in a dataset
+    train_container = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=False, num_workers=0)
+
+    # Create a container and data iterator for test 
+    test_set = torch.utils.data.TensorDataset(trans(x_test), y_test)
+    test_container = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=0)
+    
+    return train_container, test_container
--- S1/final_exercise/main.py
+++ S1/final_exercise/main.py
@@ -6,7 +6,7 @@ import click
 
 from data import mnist
 from model import MyAwesomeModel
-
+from torch import nn 
 
 @click.group()
 def cli():
@@ -15,13 +15,46 @@ def cli():
 
 @click.command()
 @click.option("--lr", default=1e-3, help='learning rate to use for training')
-def train(lr):
+@click.option("--ep", default=5, help='epochs to use for training')
+def train(lr, optimaizer=None, criterion=None, ep=5):
     print("Training day and night")
     print(lr)
 
     # TODO: Implement training loop here
+    if optimizer is None:
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+
+    if criterion is None:
+        criterion = nn.NLLLoss()
+    # Create the instance of the model 
     model = MyAwesomeModel()
+    # Load the data
     train_set, _ = mnist()
+    # set model to train 
+    model.eval()
+    for e in ep:
+        running_loss = 0
+        for images, label in train_set:
+            # Flatten images 
+            x = images.resize_(images.size()[0], 784)
+            # Set gradients to 0
+            optimaizer.zero_grad()
+            # Calculate the output
+            logits = model.forward(x)
+            # Calculate the loss 
+            loss = criterion(logits, label)
+            # Perform the backward pass to calculate the gradients 
+            loss.backward()
+            # Substract the gradient from the weights (perform a step)
+            optimaizer.step()
+            # Store the running_loss 
+            running_loss += loss.item()
+        loss_tmp = loss/len(train_set)
+        print(f"Epoch: {e+1/ep}\t loss: {loss_tmp}:3f" )
+
+
+
+
 
 
 @click.command()
